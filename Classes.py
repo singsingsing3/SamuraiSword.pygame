9550d74c1cef28c05ae92505297c9d6ee833d70d
TaeHo <otr0811@gmail.com>
2023-11-17 19:49:20 +0900

    Game_Play_Complete

--- Classes.py
+++ Classes.py
@@ -29,7 +29,7 @@ class Dot:
 
 
 #난이도 클래스
-class Difficulty: # 클래스로 찍어내니까 random값이 한판 내에서 고정값이 되어버림
+class Level: # 클래스로 찍어내니까 random값이 한판 내에서 고정값이 되어버림
     def __init__(self, level):
         if level == 1: 
           self.radius = 20 
@@ -38,14 +38,28 @@ class Difficulty: # 클래스로 찍어내니까 random값이 한판 내에서 �
           self.enemy_attack = 10 # 5~15
           self.dot_alpha = 200
           self.dot_time = 2
+          # level 별 사진
+          self.background_image = pygame.transform.scale(pygame.image.load('Images\Level_1.jpg'), (width, height)) if 'Images\Level_1.jpg' else None
+          self.background_rect = self.background_image.get_rect()
+          self.attacked_image =  pygame.transform.scale(pygame.image.load('Images\Level_1_attacked.png'), (width, height)) if 'Images\Level_1_attacked.png' else None
+          self.attacked_rect = self.attacked_image.get_rect()
+          self.defend_image =  pygame.transform.scale(pygame.image.load('Images\Level_1_defend.png'), (width, height)) if 'Images\Level_1_defend.png' else None
+          self.defend_rect = self.defend_image.get_rect()
 
         elif level == 2:
-          self.radius = 15
+          self.radius = 15 
           self.speed = 4
           self.my_attack = 10 #5~15
           self.enemy_attack = 15 #10~20
           self.dot_alpha = 150
           self.dot_time = 1.5
+
+          self.background_image = pygame.transform.scale(pygame.image.load('Images\Level_2.png'), (width, height)) if 'Images\Level_2.png' else None
+          self.background_rect = self.background_image.get_rect()
+          self.attacked_image = pygame.transform.scale(pygame.image.load('Images\Level_2_attacked.png'), (width, height)) if 'Images\Level_2_attacked.png' else None
+          self.attacked_rect = self.attacked_image.get_rect()
+          self.defend_image = pygame.transform.scale(pygame.image.load('Images\Level_2_defend.png'), (width, height)) if 'Images\Level_2_defend.png' else None
+          self.defend_rect = self.defend_image.get_rect()
           
         else: #난이도 최상
           self.radius = 10
@@ -54,3 +68,28 @@ class Difficulty: # 클래스로 찍어내니까 random값이 한판 내에서 �
           self.enemy_attack = 20 # 15~25
           self.dot_alpha = 100
           self.dot_time = 1.3
+
+          self.background_image = pygame.transform.scale(pygame.image.load('Images\Level_3.png'), (width, height)) if 'Images\Level_3.png' else None
+          self.background_rect = self.background_image.get_rect()
+          self.attacked_image = pygame.transform.scale(pygame.image.load('Images\Level_3_attacked.png'), (width, height)) if 'Images\Level_3_attacked.png' else None
+          self.attacked_rect = self.attacked_image.get_rect()
+          self.defend_image =pygame.transform.scale(pygame.image.load('Images\Level_3_defend.png'), (width, height)) if 'Images\Level_3_defend.png' else None
+          self.defend_rect = self.defend_image.get_rect()
+
+#특수효과음
+class Sound:
+    def __init__(self):
+        self.attacked_sound = pygame.mixer.Sound('SoundTrack/attacked.mp3')
+        self.died_sound = pygame.mixer.Sound('SoundTrack/died.wav')
+        self.defend_sound = pygame.mixer.Sound('SoundTrack/enemy_attack_defend.wav')
+        self.enemy_down_sound = pygame.mixer.Sound('SoundTrack/enemy_down.wav')
+        self.my_attack_sound = pygame.mixer.Sound('SoundTrack/my_attack.wav')
+
+        # Set the volume for each sound effect
+        self.attacked_sound.set_volume(0.5)
+        self.died_sound.set_volume(0.5)
+        self.defend_sound.set_volume(0.7)
+        self.enemy_down_sound.set_volume(0.5)
+        self.my_attack_sound.set_volume(0.5)
+    
+   
--- GamePlay.py
+++ GamePlay.py
@@ -2,135 +2,191 @@ import pygame
 import sys
 import random
 import time
-from Classes import Dot, Difficulty
+from Classes import Dot, Level, Sound
 
-# Pygame 초기화
+# Pygame initialization
 pygame.init()
 
-# 화면 설정
+# Initialize pygame.mixer
+pygame.mixer.init()
+
+# Load background music and set volume
+pygame.mixer.music.load('SoundTrack\Jim Yosef - Samurai [NCS Release]-[AudioTrimmer.com].mp3')
+pygame.mixer.music.set_volume(0.5)  # Adjust the volume as needed
+game_sound = Sound()
+
+# Start playing background music
+pygame.mixer.music.play(-1)  # -1 means play indefinitely
+
+# Screen settings
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Catch the Dots")
 
-# 색깔 정의
+# color definition
+BLACK =(0,0,0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-
-# 난이도 정의
-level = Difficulty(1) #레벨 설정
-
-
-
-
-# 게임 루프
-dots = []
 my_health = 100
-enemy_health = 100
-font = pygame.font.Font(None, 36)
-next_dot_time_red = time.time() + 1  # 다음 빨간 점이 나타날 시간 초기화
-next_dot_time_blue = time.time() + level.speed  # 다음 파란 점이 나타날 시간 초기화
-freeze_time = 0  # 화면이 멈춰있는 시간
+attacked = True
 
-while enemy_health > 0 and my_health > 0:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+# Outer loop for game levels
+for i in range(1, 4):
+
+    # Difficulty definition
+    level = Level(i)  # Level setting
+    
+    background_color = (BLACK)  # Black with varying alpha
+    text_color = (WHITE)  # White with varying alpha
+
+    screen.fill(background_color)  # Fill the background
+    level_font = pygame.font.Font(None, 48)
+    level_text = level_font.render("Get Ready For Level {}".format(i), True, text_color)
+    text_rect = level_text.get_rect(center=(width // 2, height // 2))
+    screen.blit(level_text, text_rect.topleft)
+      
+    pygame.display.flip()
+
+    time.sleep(3)
+
+
+    # game loop
+    dots = []
+
+    enemy_health = 100
+    font = pygame.font.Font(None, 36)
+    next_dot_time_red = time.time() + 1  # Initialize the time when the next red dot will appear
+    next_dot_time_blue = time.time() + level.speed  # Initialize the time when the next blue dot will appear
+    freeze_time = 0  # Time the screen is frozen
+
+    while enemy_health > 0 and my_health > 0:
+        screen.blit(level.background_image, level.background_rect)  # Draw the background image
+        for event in pygame.event.get():
+         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
 
-    # 새로운 빨간 점 생성 (1초에 한 번씩)
-    if time.time() > next_dot_time_red and time.time() > freeze_time:
-        dot = Dot(random.randint(20, width - 20),
-                  random.randint(20, height - 20), color=RED, alpha=0, radius=level.radius)
-        dots.append(dot)
-        next_dot_time_red = time.time() + 1  # 다음 빨간 점이 나타날 시간 업데이트
-
-    # 새로운 파란 점 생성 
-    if time.time() > next_dot_time_blue and time.time() > freeze_time:
-        # 중복된 파란 점을 방지하기 위해 이미 존재하는 파란 점인지 확인
-        existing_blue_dots = [dot for dot in dots if dot.color == BLUE]
-        if not existing_blue_dots:
-            dot = Dot(random.randint(20, width - 20),  # 점이 화면에 잘리지 않도록 범위 설정
-                      random.randint(20, height - 20), color=BLUE,  radius=level.radius,alpha=0)
+     # Create a new red dot (once per second)
+        if time.time() > next_dot_time_red and time.time() > freeze_time:
+            dot = Dot(random.randint(20, width - 30),
+                  random.randint(20, height - 30), color=RED, alpha=0, radius=level.radius)
             dots.append(dot)
-            next_dot_time_blue = time.time() + level.speed  # 다음 파란 점이 나타날 시간 업데이트
+            next_dot_time_red = time.time() + 1  # Update the time when the next red dot will appear
 
-    # 화면 업데이트
+        # Create a new blue dot
+        if time.time() > next_dot_time_blue and time.time() > freeze_time:
+            # Check if the blue dot already exists to prevent duplicate blue dots
+            existing_blue_dots = [dot for dot in dots if dot.color == BLUE]
+            if not existing_blue_dots:
+                dot = Dot(random.randint(20, width - 20),  # Set the range so that the dot is not cut off on the screen
+                        random.randint(20, height - 20), color=BLUE, radius=level.radius, alpha=0)
+                dots.append(dot)
+                next_dot_time_blue = time.time() + level.speed  # Update the time when the next blue dot will appear
+
+        
+
+        # Draw all points
+        for dot in dots:
+            dot.draw()
+            #Fade in effect
+            if dot.color and dot.alpha < level.dot_alpha:
+                dot.alpha += 2
+
+        # Display score
+        my_text = font.render("Your HP: {}".format(my_health), True, RED)
+        enemy_text = font.render("Enemy HP: {}".format(enemy_health), True, BLUE)
+        screen.blit(my_text, (10, 10))
+        screen.blit(enemy_text, (width - 200, 10))
+
+        # The dot disappears some seconds after it is created
+        for dot in dots.copy():
+            if time.time() - dot.creation_time > level.dot_time:  # If the blue dot cannot be removed and disappears
+                if dot.color == BLUE:  # If it is a blue dot
+                    dots.clear()  # remove all dots
+                    my_health -= int(level.enemy_attack + random.uniform(-5, 6))
+                    attacked = True   
+                    freeze_time = time.time() + 1  # Freeze the screen for 2 seconds
+                    game_sound.attacked_sound.play()
+                elif dot.color == RED:  # If red dot
+                    dots.remove(dot)
+
+        
+
+        # Mouse event handling
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                for dot in dots.copy():
+                    if dot.color == BLUE and 0 <= mouse_y <= 50:  # Defend opponent's attack
+                        game_sound.defend_sound.play()
+                        attacked = False
+                        dots.clear()  # remove all dots
+                        freeze_time = time.time() + 1  # Freeze the screen for 2 seconds
+                    
+                    elif dot.color == RED:  # If red dot
+                        distance = pygame.math.Vector2(
+                            dot.x - mouse_x, dot.y - mouse_y).length()
+                        if distance < dot.radius:
+                            dots.remove(dot)
+                            # Reduce enemy health
+                            enemy_health -= int(level.my_attack + random.uniform(-5, 6))
+                            game_sound.my_attack_sound.play()
+
+       
+    
+
+        # While the screen is still for 2 seconds
+        if time.time() < freeze_time and attacked:
+            screen.blit(level.attacked_image, level.attacked_rect)  # Draw the background image
+            pygame.display.flip()  # Screen update
+            continue  # do not repeat the game loop
+        elif time.time() < freeze_time and  not attacked:
+            screen.blit(level.defend_image, level.defend_rect)  # Draw the background image
+            pygame.display.flip()  # Screen update
+            continue  # do not repeat the game loop
+
+        # Screen update
+        pygame.display.flip()
+
+
+    # Display the latest health value after the game ends
     screen.fill(WHITE)
-
-    # 모든 점 그리기
-    for dot in dots:
-        dot.draw()
-        # 페이드 인 효과
-        if dot.color and dot.alpha < level.dot_alpha:
-            dot.alpha += 2
-
-    # 점수 표시
     my_text = font.render("Your HP: {}".format(my_health), True, RED)
     enemy_text = font.render("Enemy HP: {}".format(enemy_health), True, BLUE)
     screen.blit(my_text, (10, 10))
-    screen.blit(enemy_text, (width-200, 10))
+    screen.blit(enemy_text, (width - 200, 10))
 
-    # 점이 생성된 후 2초 후에 사라짐
-    for dot in dots.copy():
-        if time.time() - dot.creation_time > level.dot_time:  # 파란점이 제거되지 못하고 사라진 경우
-            if dot.color == BLUE:  # 파란 점인 경우
-                dots.clear()  # 모든 점 제거                
-                my_health -= int(level.enemy_attack + random.uniform(-5, 6))
-                freeze_time = time.time() + 2  # 화면을 2초 동안 멈춤
-            elif dot.color == RED:  # 빨간 점인 경우
-                dots.remove(dot)
+    if my_health <= 0:
+        lose_font = pygame.font.Font(None, 48)
+        screen.fill(background_color)
+        lose = font.render("You Died!", True, RED)
+        game_sound.died_sound.play()
+        text_rect = lose.get_rect(center=(width // 2, height // 2))   
+        screen.blit(lose, text_rect.topleft)
 
-    # 마우스 이벤트 처리
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEMOTION:
-            for dot in dots.copy():
-                if dot.color == BLUE and 0 <= mouse_y <= 10:  # 상대 공격 방어
-                    dots.clear()  # 모든 점 제거
-                    freeze_time = time.time() + 2  # 화면을 2초 동안 멈춤
-                elif dot.color == RED:  # 빨간 점이면
-                    distance = pygame.math.Vector2(
-                        dot.x - mouse_x, dot.y - mouse_y).length()
-                    if distance < dot.radius:
-                        dots.remove(dot)
-                          # 적 체력 감소
-                        enemy_health -= int(level.my_attack + random.uniform(-5, 6))
+    elif enemy_health <= 0:
+        # Increase font size to 48 (adjust as needed)
+        win_font = pygame.font.Font(None, 48)
+        game_sound.enemy_down_sound.play()
+        screen.fill(background_color)
+        win = win_font.render("You Won!", True, BLUE)
+        text_rect = win.get_rect(center=(width // 2, height // 2))
+        screen.blit(win, text_rect.topleft)
 
-    # 2초동안 화면이 멈춰있는 동안
-    if time.time() < freeze_time:
-        pygame.display.flip()  # 화면 갱신
-        continue  # 게임 루프 반복하지 않음
-
-    # 화면 업데이트
-    pygame.display.flip()
-
-
-if my_health <= 0:   
-    lose_font = pygame.font.Font(None, 48)
-    lose = font.render("You Died!", True, RED)
-    text_rect = lose.get_rect(center=(width // 2, height // 2))
-    screen.blit(lose, text_rect.topleft)
+    pygame.display.flip()  # Display the game over message
+    time.sleep(4)  # Pause for 4 seconds before moving to the next level
     
-elif enemy_health <= 0:  
-    # Increase font size to 48 (adjust as needed)
-    win_font = pygame.font.Font(None, 48)
-    win = win_font.render("You Won!", True, BLUE)
-    text_rect = win.get_rect(center=(width // 2, height // 2))
-    screen.blit(win, text_rect.topleft)
 
-pygame.display.flip()  # Display the game over message
 
-# Event handling loop to keep the screen on
-running = True
-
+# Game Finished
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN: # 키 누를시 게임종료
+        elif event.type == pygame.KEYDOWN:  # Game ends when key is pressed
             running = False
-
+pygame.mixer.music.stop() # 음악종료
 pygame.quit()
-
